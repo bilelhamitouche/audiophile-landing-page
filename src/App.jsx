2537d35eb80c255a2e0f3b88e8cf5ad006da9bdf
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-07 17:26:14 +0100

    added tailwindcss configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="grid place-items-center h-screen text-red-800">
+      <h1 className="text-xl font-bold">Hello world</h1>
     </div>
   );
 }
