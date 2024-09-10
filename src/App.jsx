bcc239643b50253acd0631537b1ec2e294046588
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-10 07:05:07 +0100

    added ProductCategories and modified Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ProductCategories from "./components/ProductCategories";
 
 function App() {
   return (
-    <header className="bg-zinc-800">
-      <Navbar />
-      <Hero />
-    </header>
+    <>
+      <header className="bg-zinc-800">
+        <Navbar />
+        <Hero />
+      </header>
+      <ProductCategories />
+    </>
   );
 }
 
