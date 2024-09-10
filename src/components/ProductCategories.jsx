24def3c7fca47edbf2441a9c2885ee3689a1778a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-10 07:03:59 +0100

    added ProductCategories component

--- src/components/ProductCategories.jsx
+++ src/components/ProductCategories.jsx
@@ -1,5 +1,25 @@
+import ProductCategoryCard from "./ProductCategoryCard";
+import HeadsetImage from "../assets/headset.png";
+import SpeakerImage from "../assets/speaker.png";
+import EarphoneImage from "../assets/earphone.png";
+
 function ProductCategories() {
-  return <section className="p-8 bg-white md:container"></section>;
+  return (
+    <section className="grid grid-cols-1 grid-rows-3 gap-16 justify-center items-center px-8 pt-80 pb-36 mx-auto bg-gray-50 sm:grid-cols-3 md:container">
+      <ProductCategoryCard
+        productCategoryName="headphones"
+        productCategoryImage={HeadsetImage}
+      />
+      <ProductCategoryCard
+        productCategoryName="speakers"
+        productCategoryImage={SpeakerImage}
+      />
+      <ProductCategoryCard
+        productCategoryName="earphones"
+        productCategoryImage={EarphoneImage}
+      />
+    </section>
+  );
 }
 
 export default ProductCategories;
