300694da93679cda3a3e1f29b964d1783c01dd3f
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-11 19:30:40 +0100

    added earphone section to ProductList component

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,9 +1,10 @@
 import SpeakerImage from "../assets/speaker2.png";
+import EarphoneImage from "../assets/earphone2.png";
 
 function ProductsList() {
   return (
     <section className="flex flex-col gap-10 p-8 mx-auto sm:container">
-      <div className="flex relative flex-col bg-orange-400 rounded-lg sm:flex-row">
+      <div className="flex flex-col bg-orange-400 rounded-lg sm:flex-row">
         <img
           src={SpeakerImage}
           alt="speaker image"
@@ -11,10 +12,10 @@ function ProductsList() {
         />
         <div className="flex flex-col gap-8 justify-center items-start">
           <h2 className="flex flex-col">
-            <span className="text-6xl font-semibold text-white uppercase">
+            <span className="text-5xl font-semibold text-white uppercase">
               zx9
             </span>
-            <span className="text-6xl font-semibold text-white uppercase">
+            <span className="text-5xl font-semibold text-white uppercase">
               speaker
             </span>
           </h2>
@@ -27,9 +28,9 @@ function ProductsList() {
           </a>
         </div>
       </div>
-      <div className="flex relative flex-col pl-24 bg-gray-300 rounded-lg sm:flex-row">
+      <div className="flex flex-col pl-24 bg-gray-300 rounded-lg sm:flex-row">
         <div className="flex flex-col gap-8 justify-center items-start">
-          <h2 className="text-6xl font-semibold uppercase">zx7 speaker</h2>
+          <h2 className="text-5xl font-semibold uppercase">zx7 speaker</h2>
           <a className="py-4 px-8 text-black uppercase bg-transparent border-2 border-black text-md">
             see product
           </a>
@@ -39,6 +40,21 @@ function ProductsList() {
           alt="speaker image"
           className="object-fill w-3/6"
         />
+      </div>
+      <div className="flex flex-col gap-8 rounded-lg sm:flex-row">
+        <img
+          src={EarphoneImage}
+          alt="earphone image"
+          className="object-fill w-3/6 bg-black rounded-lg"
+        />
+        <div className="flex flex-col gap-8 justify-center items-start pr-28 pl-24 bg-gray-200 rounded-lg">
+          <h2 className="text-5xl font-semibold text-black uppercase">
+            yx1 earphones
+          </h2>
+          <a className="py-4 px-8 text-black uppercase bg-transparent border-2 border-black text-md">
+            see product
+          </a>
+        </div>
       </div>
     </section>
   );
