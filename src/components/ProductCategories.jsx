194757b102868a3fe5d18fa23e83550d2a9ec078
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-09-15 10:28:57 +0100

    Make the categories respond better in medium size

--- src/components/ProductCategories.jsx
+++ src/components/ProductCategories.jsx
@@ -5,7 +5,7 @@ import EarphoneImage from "../assets/earphone.png";
 
 function ProductCategories() {
   return (
-    <section className="grid grid-cols-1 grid-rows-3 gap-16 justify-center items-center px-8 pt-80 mx-auto -mb-48 bg-gray-50 sm:grid-cols-3 md:container">
+    <section className="grid grid-cols-1 grid-rows-3 gap-32 justify-center items-center px-8 pt-80 mx-auto mb-60 bg-gray-50 md:container md:grid-cols-3 md:gap-16 md:-mb-48">
       <ProductCategoryCard
         productCategoryName="headphones"
         productCategoryImage={HeadsetImage}
